add1aa84ff119fd207937fd9ec8854ea82de6538
Luis <lamadoalvarez@gmail.com>
2025-03-09 19:02:22 -0600

    Move listLogs to API in TRPC, as well as run the dataconnect in the backend

--- tenor_web/src/utils/dataconnect.js
+++ tenor_web/src/utils/dataconnect.js
@@ -1,11 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { connectorConfig } from "@firebasegen/tenor-muse";
-import { connectDataConnectEmulator, getDataConnect } from 'firebase/data-connect';
+import {
+  connectDataConnectEmulator,
+  getDataConnect,
+} from "firebase/data-connect";
 import { env } from "~/env";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    projectId: env.AUTH_FIREBASE_PROJECT_ID
+  projectId: env.AUTH_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -14,4 +17,3 @@ export const dataConnect = getDataConnect(app, connectorConfig);
 connectDataConnectEmulator(dataConnect, "localhost", 9399);
 
 // Make calls from your app
-  
