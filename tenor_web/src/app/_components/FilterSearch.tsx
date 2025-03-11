ae6344f03ded827de873edb1947deda32fd68bcb
NicoM3Nd0Z4 <a00835649@tec.mx>
2025-03-11 12:27:05 -0600

    Create a component to have a search bar in the projects list

---
+++ tenor_web/src/app/_components/FilterSearch.tsx
@@ -0,0 +1,32 @@
+import {ChangeEvent, useState} from 'react';
+
+interface FilterSearchProps {
+  list: string[];
+  onSearch: (searchTerm: string[]) => void;
+}
+
+export function FilterSearch({list, onSearch}: FilterSearchProps) {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value.toLowerCase();
+    setSearchQuery(query);
+
+    if (query === "") {
+      onSearch(list);
+    } else {
+      const filtered = list.filter((item) => item.toLowerCase().includes(query));
+      onSearch(filtered);
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      value={searchQuery}
+      onChange={handleSearch}
+      placeholder="Search projects..."
+      className="search-bar"
+    />
+  )
+};
