47903e8948e95a52932470c690ec1934af57d38a
luis r <luisresendez_@hotmail.com>
2025-03-11 19:17:06 -0600

    Frida API adaptation

--- tenor_web/src/server/api/routers/frida.ts
+++ tenor_web/src/server/api/routers/frida.ts
@@ -8,15 +8,19 @@ import {
 
 export const fridaRouter = createTRPCRouter({
   generateREQ: publicProcedure.input(z.string()).query(async ({input}) => {
-    const result = await fetch('http://localhost:6453/generateREQ', 
+    const result = await fetch('https://stk-formador-25.azurewebsites.net/epics/generate-from-prompt/', 
     {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        "context": input
-    })});
+        prompt: 'Generate only a list of 10 functional requirements and 3 non-functional requirements for software development based on the following context: ${context}. Provide only the list, without any comments, opinions, explanations, or introductions.',
+        data: {
+          context : input
+        }
+      }),
+    });
     return await result.json();
   })
 });
