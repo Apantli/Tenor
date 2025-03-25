ef9ae0e74807bb003f1ea074bd38ec3c7bb509fd
Luis <lamadoalvarez@gmail.com>
2025-03-24 23:42:02 -0600

    Add navbar Dashboard and project name spaces

--- tenor_web/src/app/(logged)/(dashboard)/layout.tsx
+++ tenor_web/src/app/(logged)/(dashboard)/layout.tsx
@@ -1,13 +1,18 @@
 import "~/styles/globals.css";
 
 import Navbar from "../../_components/Navbar";
+import Link from "next/link";
 
 export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
-      <Navbar />
+      <Navbar>
+        <Link href="/" className="font-medium">
+          Dashboard
+        </Link>
+      </Navbar>
       <main className="m-6 p-4">{children}</main>
     </>
   );
--- tenor_web/src/app/(logged)/project/[projectId]/layout.tsx
+++ tenor_web/src/app/(logged)/project/[projectId]/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { PropsWithChildren } from "react";
 import Navbar from "~/app/_components/Navbar";
 import Tabbar from "~/app/_components/Tabbar";
@@ -5,7 +6,14 @@ import Tabbar from "~/app/_components/Tabbar";
 export default function ProjectLayout({ children }: PropsWithChildren) {
   return (
     <div>
-      <Navbar />
+      <Navbar>
+        <div className="flex gap-3">
+          <Link href="/" className="font-bold">
+            Projects
+          </Link>
+          <span>/ ProjectName</span>
+        </div>
+      </Navbar>
       <Tabbar />
       <main className="m-6 p-4">{children}</main>
     </div>
