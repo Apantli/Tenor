17646ffea32e793c8db3ccb6a2e8839294604a66
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-03-28 14:43:20 -0600

    debug workflow

--- tenor_web/cypress/e2e/app.cy.ts
+++ tenor_web/cypress/e2e/app.cy.ts
@@ -1,6 +1,10 @@
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("http://localhost:3000/");
+    cy.screenshot("full-page", { capture: "fullPage" });
+    cy.document().then((doc) => {
+      cy.log(doc.documentElement.outerHTML);
+    });
     cy.contains("Don't have an account?").click();
     cy.url().should("include", "/register");
     cy.contains("Already have an account?").click();
