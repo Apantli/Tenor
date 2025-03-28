008b75285fb4d1f73da082b628ad8e4a13864c93
Luis <lamadoalvarez@gmail.com>
2025-03-28 15:01:16 -0600

    keyword

--- tenor_web/cypress/e2e/app.cy.ts
+++ tenor_web/cypress/e2e/app.cy.ts
@@ -2,6 +2,7 @@ describe("template spec", () => {
   it("passes", () => {
     cy.visit("http://localhost:3000/");
     cy.document().then((doc) => {
+      cy.log("EL DIABLO");
       cy.log(doc.documentElement.outerHTML);
     });
     cy.contains("Don't have an account?").click();
