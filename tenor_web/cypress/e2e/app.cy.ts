4d3028209e3daade743fd1a6497fdc9d48796c2f
Luis <lamadoalvarez@gmail.com>
2025-03-28 15:23:05 -0600

    YES YES

--- tenor_web/cypress/e2e/app.cy.ts
+++ tenor_web/cypress/e2e/app.cy.ts
@@ -7,12 +7,4 @@ describe("template spec", () => {
     cy.contains("Already have an account?", { timeout: 10000 }).click();
     cy.url().should("include", "/login");
   });
-  it("fails", () => {
-    // First ensure the page is loaded
-    cy.visit("/");
-    cy.contains("Don't have an accounts?", { timeout: 10000 }).click();
-    cy.url().should("include", "/register");
-    cy.contains("Already have an account?", { timeout: 10000 }).click();
-    cy.url().should("include", "/login");
-  });
 });
