3f38c81b0f7baf0e94d7a52d5f963978dbd247b5
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-04-04 11:09:38 -0600

    fix: open project onClick

--- tenor_web/src/app/(logged)/(dashboard)/page.tsx
+++ tenor_web/src/app/(logged)/(dashboard)/page.tsx
@@ -94,8 +94,9 @@ function ProjectList() {
         {filteredProjects && filteredProjects?.length > 0 ? (
           filteredProjects?.map((project) => (
             <li
-              className="flex h-full max-w-[490px] justify-start border-b-2 py-[8]"
+              className="flex h-full max-w-[490px] justify-start border-b-2 py-[8] hover:cursor-pointer"
               key={project.id}
+              onClick={() => handleOpenProject(project.id)}
             >
               <div className="m-[10px] flex h-24 max-h-[66px] w-24 max-w-[66px] items-center justify-center rounded-md bg-blue-500">
                 <img
