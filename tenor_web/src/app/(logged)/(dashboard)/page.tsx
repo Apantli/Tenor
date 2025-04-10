11852a3020983cd28239e03d637e1cc6dbf084fb
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-04-10 11:41:13 -0600

    style: ui

--- tenor_web/src/app/(logged)/(dashboard)/page.tsx
+++ tenor_web/src/app/(logged)/(dashboard)/page.tsx
@@ -2,20 +2,17 @@
 
 import { api } from "~/trpc/react";
 import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
-import { FilterSearch } from "~/app/_components/FilterSearch";
+import { useState, type ChangeEventHandler } from "react";
+import SearchBar from "~/app/_components/SearchBar";
 import PrimaryButton from "~/app/_components/buttons/PrimaryButton";
+import LoadingSpinner from "~/app/_components/LoadingSpinner";
 
 export default function ProjectPage() {
   return (
     <div className="flex h-full w-full flex-row">
-      <div className="w-full">
-        <div className="">
-          <h1 className="text-xl font-bold">Projects</h1>
-        </div>
-        <div className="">
-          <ProjectList />
-        </div>
+      <div className="w-1/2">
+        <h1 className="mb-3 text-3xl font-semibold">Projects</h1>
+        <ProjectList />
       </div>
       <div className="w-full"></div>
     </div>
@@ -29,13 +26,7 @@ const CreateNewProject = () => {
   };
 
   return (
-    <PrimaryButton
-      className={"h-full w-full max-w-[103px] self-center text-xs"}
-      onClick={handleCreateProject}
-    >
-      {" "}
-      + New project{" "}
-    </PrimaryButton>
+    <PrimaryButton onClick={handleCreateProject}> + New project </PrimaryButton>
   );
 };
 
@@ -45,7 +36,11 @@ function ProjectList() {
     isLoading,
     error,
   } = api.projects.listProjects.useQuery();
-  const [filteredProjects, setFilteredProjects] = useState<typeof projects>([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleUpdateSearch: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchValue(e.target.value);
+  };
 
   /**
    * * This function is used to open a project when the user clicks on the project image.
@@ -57,37 +52,30 @@ function ProjectList() {
     router.push(`/project/${projectId}`);
   };
 
-  useEffect(() => {
-    if (projects) {
-      setFilteredProjects(projects);
-    }
-  }, [projects]);
-
   if (isLoading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="flex flex-row gap-3 align-middle">
+        <LoadingSpinner />
+        <p className="text-lg font-semibold">Loading your projects...</p>
+      </div>
+    );
   }
 
   if (error?.data?.code == "UNAUTHORIZED") {
     return <p>Log in to view this information</p>;
   }
 
-  const handleFilter = (filterList: string[]) => {
-    if (filterList.length === 0) {
-      setFilteredProjects(projects ?? []);
-    } else {
-      setFilteredProjects(
-        projects?.filter((p) => filterList.includes(p.name)) ?? [],
-      );
-    }
-  };
+  const filteredProjects = projects?.filter((project) => {
+    return project.name.toLowerCase().includes(searchValue.toLowerCase());
+  });
 
   return (
-    <div>
-      <div className="flex h-full max-h-[33px] w-full max-w-[490px] justify-between">
-        <FilterSearch
-          list={projects?.map((p) => p.name) ?? []}
-          onSearch={handleFilter}
-          placeholder="Search project..."
+    <div className="mr-10">
+      <div className="order-b-2 flex h-full w-full justify-between gap-x-3 border-b-2 pb-3">
+        <SearchBar
+          searchValue={searchValue}
+          handleUpdateSearch={handleUpdateSearch}
+          placeholder="Find a project..."
         />
         <CreateNewProject />
       </div>
@@ -95,11 +83,11 @@ function ProjectList() {
         {filteredProjects && filteredProjects?.length > 0 ? (
           filteredProjects?.map((project) => (
             <li
-              className="flex h-full max-w-[490px] justify-start border-b-2 py-[8] hover:cursor-pointer"
+              className="flex h-full flex-row justify-start border-b-2 py-[16px] hover:cursor-pointer"
               key={project.id}
               onClick={() => handleOpenProject(project.id)}
             >
-              <div className="m-[10px] flex h-[80px] w-[80px] items-center justify-center overflow-hidden rounded-md bg-white">
+              <div className="m-[10px] flex h-[80px] w-[80px] items-center justify-center overflow-hidden rounded-md border-2 bg-white">
                 <img
                   className="h-full w-full rounded-md object-contain p-[4px]"
                   src={project.logo}
@@ -107,22 +95,30 @@ function ProjectList() {
                 />
               </div>
               <div className="ml-2 flex max-h-full w-full flex-col justify-start">
-                <h3 className="my-[7px] text-lg font-semibold">
+                <h3 className="my-[7px] w-60 truncate text-lg font-semibold">
                   {project.name}
                 </h3>
-                <p className="text-sm">{project.description}</p>
+                <p className="max-w-full truncate text-base">
+                  {project.description}
+                </p>
               </div>
             </li>
           ))
         ) : (
-          <li className="flex h-full max-w-[490px] justify-start border-b-2 py-[8]">
-            <div className="mt-4">
+          <li className="flex h-full justify-start border-b-2">
+            <div className="py-[20px]">
               <p className="text-gray-500">No projects found.</p>
 
-              <p className="text-sm text-gray-500">
-                Try creating a project or ask a project owner to add you to a
-                project.{" "}
-              </p>
+              {(projects?.length ?? 0) > 0 ? (
+                <p className="text-sm text-gray-500">
+                  Try changing the search query.
+                </p>
+              ) : (
+                <p className="text-sm text-gray-500">
+                  Try creating a project or ask a project owner to add you to a
+                  project.{" "}
+                </p>
+              )}
             </div>
           </li>
         )}
