270f43d736d70b0783ba797f27e75e6a1f1e7b72
luis r <luisresendez_@hotmail.com>
2025-04-25 14:46:18 -0600

    pr feedback

--- tenor_web/src/app/(logged)/create-project/page.tsx
+++ tenor_web/src/app/(logged)/create-project/page.tsx
@@ -125,7 +125,7 @@ export default function ProjectCreator() {
     );
   };
 
-  const logoSizeLimit = 3 * 1024 * 1024; // 5MB in bytes
+  const logoSizeLimit = 3 * 1024 * 1024; // 3MB in bytes
   const logoMaxDimensions = 1024; // Maximum width/height in pixels
   const [icon, setImage] = useState<File | null>(null);
   const [isValidatingImage, setIsValidatingImage] = useState(false);
