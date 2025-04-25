7fda1d844d36d657bf1cfd03c9843e45e7c7ce9a
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-04-25 09:32:58 -0600

    temp test

--- tenor_web/src/app/(logged)/project/[projectId]/page.tsx
+++ tenor_web/src/app/(logged)/project/[projectId]/page.tsx
@@ -1,14 +1,57 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { useState } from "react";
+import PrimaryButton from "~/app/_components/buttons/PrimaryButton";
+import { api } from "~/trpc/react";
 
 export default function ProjectOverview() {
   const { projectId } = useParams();
+  const [messages, setMessages] = useState<string[]>([]);
+  const { mutateAsync: generateResponse, isPending: generating } =
+    api.frida.genericAIResponse.useMutation();
 
   return (
     <div>
       <h1 className="text-2xl font-semibold">Project overview</h1>
       <p>Project Id: {projectId}</p>
+      {messages.length > 0 ? (
+        <div className="mt-4">
+          <h2 className="text-xl font-semibold">Message</h2>
+          <ul className="list-disc pl-5">
+            {messages.map((msg, index) => (
+              <li key={index} className="mt-2">
+                {msg}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p className="mt-4">
+          No messages yet. You can generate a new project by entering a name
+          below.
+        </p>
+      )}
+
+      <input
+        type="text"
+        className="mt-4 rounded-md border-2 border-gray-300 p-2"
+        placeholder="Enter project name"
+      />
+      <PrimaryButton
+        onClick={async () => {
+          const response = await generateResponse({
+            schema: "string",
+            context: "string",
+            messages: ["string"],
+            prompt: "string",
+          });
+          setMessages([...messages, response]);
+        }}
+        className="mt-4"
+      >
+        Generate
+      </PrimaryButton>
     </div>
   );
 }
--- tenor_web/src/server/api/routers/frida.ts
+++ tenor_web/src/server/api/routers/frida.ts
@@ -1,32 +1,69 @@
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  publicProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { zodToJsonSchema } from "zod-to-json-schema";
 
 interface APIResponse {
-  success: boolean,
-  data: string,
-  message: string,
-  error: unknown,
+  success: boolean;
+  data: string;
+  message: string;
+  error: unknown;
 }
 
 export const fridaRouter = createTRPCRouter({
-  generateREQ: publicProcedure.input(z.string()).query(async ({input}) => {
-    const result = await fetch('https://stk-formador-25.azurewebsites.net/epics/generate-from-prompt/', 
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+  generateREQ: publicProcedure.input(z.string()).query(async ({ input }) => {
+    const result = await fetch(
+      "https://stk-formador-25.azurewebsites.net/epics/generate-from-prompt/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt:
+            "Generate only a list of 10 functional requirements and 3 non-functional requirements for software development based on the following context: ${context}. Provide only the list, without any comments, opinions, explanations, or introductions.",
+          data: {
+            context: input,
+          },
+        }),
       },
-      body: JSON.stringify({
-        prompt: 'Generate only a list of 10 functional requirements and 3 non-functional requirements for software development based on the following context: ${context}. Provide only the list, without any comments, opinions, explanations, or introductions.',
-        data: {
-          context : input
-        }
+    );
+    return (await result.json()) as APIResponse;
+  }),
+  genericAIResponse: publicProcedure
+    .input(
+      z.object({
+        schema: z.string(),
+        context: z.string(),
+        messages: z.array(z.string()),
+        prompt: z.string(),
       }),
-    });
-    return await result.json() as APIResponse;
-  })
+    )
+    .mutation(async ({ input }) => {
+      const schema = z.object({
+        response: z.string(),
+      });
+      const jsonSchema = zodToJsonSchema(schema, "Response");
+      console.log(jsonSchema);
+
+      // const result = await fetch(
+      //   "https://stk-formador-25.azurewebsites.net/epics/generate-from-prompt/",
+      //   {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //     body: JSON.stringify({
+      //       prompt: input.prompt,
+      //       data: {
+      //         context: input,
+      //       },
+      //     }),
+      //   },
+      // );
+
+      // const res = (await result.json()) as APIResponse;
+      // console.log(res);
+      return "res";
+    }),
 });
