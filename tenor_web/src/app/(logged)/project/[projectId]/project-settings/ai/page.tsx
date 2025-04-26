67931b7b97a66e6ef2a9ca4df8a68d883efa3adf
Alonso Huerta <alonso12_284@outlook.com>
2025-04-26 09:29:13 -0600

    inputs

--- tenor_web/src/app/(logged)/project/[projectId]/project-settings/ai/page.tsx
+++ tenor_web/src/app/(logged)/project/[projectId]/project-settings/ai/page.tsx
@@ -1,5 +1,44 @@
 "use client";
 
+import FileList from "~/app/_components/inputs/FileList";
+import InputTextAreaField from "~/app/_components/inputs/InputTextAreaField";
+import LinkList from "~/app/_components/inputs/LinkList";
+
 export default function ProjectAIConfig() {
-  return <div></div>;
+  return (
+    <div className="flex h-full w-full flex-col">
+      <div className="flex flex-row justify-between">
+        <h1 className="mb-4 text-3xl font-semibold">AI Context</h1>
+      </div>
+      {/* {teamMembers && userTypes ? ( */}
+      <InputTextAreaField label="Project Context"></InputTextAreaField>
+      <FileList
+        label={"Context Files"}
+        files={[]}
+        memoryLimit={0}
+        handleFileAdd={function (files: File[]): void {
+          throw new Error("Function not implemented.");
+        }}
+        handleFileRemove={function (file: File): void {
+          throw new Error("Function not implemented.");
+        }}
+      ></FileList>
+      <LinkList
+        label={"Context Links"}
+        links={[]}
+        handleLinkAdd={function (link: string): void {
+          throw new Error("Function not implemented.");
+        }}
+        handleLinkRemove={function (link: string): void {
+          throw new Error("Function not implemented.");
+        }}
+      ></LinkList>
+      {/* ) : (
+        <div className="mt-5 flex flex-row gap-x-3">
+          <LoadingSpinner />
+          <p className="text-lg font-bold">Loading...</p>
+        </div>
+      )} */}
+    </div>
+  );
 }
