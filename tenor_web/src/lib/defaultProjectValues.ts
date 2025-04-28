6900ea6193ff5a2db7a6e97fcd902bc55f7ad6b8
Sergio Gonzalez Barraza <114702763+SergioGzzBrz@users.noreply.github.com>
2025-04-27 19:32:32 -0600

    Add default values

---
+++ tenor_web/src/lib/defaultProjectValues.ts
@@ -0,0 +1,14 @@
+// FIXME: Move all other defaults to this file
+export const defaultRoleList = [
+  { id: "admin", label: "Admin" },
+  { id: "developer", label: "Developer" },
+  { id: "viewer", label: "Viewer" },
+];
+
+export const emptyRole = {
+  id: "none",
+  label: "No role",
+};
+
+export const defaultMaximumSprintStoryPoints = 300;
+export const defaultSprintDuration = 7;
