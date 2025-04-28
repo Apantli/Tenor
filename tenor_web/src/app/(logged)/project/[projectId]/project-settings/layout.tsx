863e7280e1ed7ba1eb9133a782f07fababc0dac5
Luis <lamadoalvarez@gmail.com>
2025-04-28 11:57:25 -0600

    Confirmation messages on settings

--- tenor_web/src/app/(logged)/project/[projectId]/project-settings/layout.tsx
+++ tenor_web/src/app/(logged)/project/[projectId]/project-settings/layout.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import { type PropsWithChildren, useRef } from "react";
+import { type PropsWithChildren } from "react";
 import SettingsIcon from "@mui/icons-material/Settings";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
 import LabelOutlinedIcon from "@mui/icons-material/LabelOutlined";
 import RouteIcon from "@mui/icons-material/Route";
 import { cn } from "~/lib/utils";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import path from "path";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import { ModificationContext } from "~/app/_hooks/useModification";
-import useConfirmation from "~/app/_hooks/useConfirmation";
+import InterceptedLink from "~/app/_components/InterceptableLink";
 const joinPath = (...paths: string[]) => {
   const joined = path.join(...paths);
   return joined.endsWith("/") ? joined.slice(0, -1) : joined;
@@ -19,14 +18,6 @@ const joinPath = (...paths: string[]) => {
 
 export default function ProjectSettingsLayout({ children }: PropsWithChildren) {
   const pathname = usePathname();
-  const router = useRouter();
-
-  const isModified = useRef(false);
-  const confirm = useConfirmation();
-
-  const setIsModified = (value: boolean) => {
-    isModified.current = value;
-  };
 
   const pages = [
     { title: "General", link: "/", icon: SettingsIcon },
@@ -50,27 +41,14 @@ export default function ProjectSettingsLayout({ children }: PropsWithChildren) {
       <div className="flex h-full w-[450px] basis-1/4 flex-col border-r-2 pr-10">
         <h1 className="mb-5 text-3xl font-semibold">Project Settings</h1>
         {pages.map(({ title, link, icon: Icon }, i) => (
-          <button
+          <InterceptedLink
             key={link}
             className={cn(
               "flex items-center gap-3 border-t-2 p-4 hover:bg-gray-100",
               i === pages.length - 1 && "border-b-2",
               joinPath(rootPath, link) === pathname && "bg-gray-100",
             )}
-            onClick={async () => {
-              if (joinPath(rootPath, link) === pathname) return;
-              if (isModified.current) {
-                const confirmation = await confirm(
-                  "Are you sure?",
-                  "Your changes will be discarded.",
-                  "Discard changes",
-                  "Keep Editing",
-                );
-                if (!confirmation) return;
-              }
-              isModified.current = false;
-              router.push(joinPath(rootPath, link));
-            }}
+            href={joinPath(rootPath, link)}
           >
             <Icon fontSize="large" />
             <span
@@ -84,14 +62,10 @@ export default function ProjectSettingsLayout({ children }: PropsWithChildren) {
             {joinPath(rootPath, link) !== pathname && (
               <ArrowForwardIosIcon className="ml-auto text-gray-300" />
             )}
-          </button>
+          </InterceptedLink>
         ))}
       </div>
-      <div className="ml-10 flex-1 overflow-auto">
-        <ModificationContext.Provider value={{ setIsModified, isModified }}>
-          {children}
-        </ModificationContext.Provider>
-      </div>
+      <div className="ml-10 flex-1 overflow-auto">{children}</div>
     </div>
   );
 }
