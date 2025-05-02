b2af40fa35716b82e83c373585b8893a76749e48
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-05-01 23:11:29 -0600

    url fix

--- tenor_web/src/utils/webcontent.ts
+++ tenor_web/src/utils/webcontent.ts
@@ -1,11 +1,19 @@
 export const fetchHTML = async (url: string): Promise<string> => {
+  const normalizedUrl = normalizeUrl(url);
+
   try {
-    const response = await fetch(url, {
+    const response = await fetch(normalizedUrl, {
       method: "GET",
       headers: {
         "Content-Type": "text/html",
-        "User-Agent": "Mozilla/5.0",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        Referer: "https://www.google.com/",
       },
+      credentials: "omit",
     });
 
     if (!response.ok) {
@@ -17,6 +25,17 @@ export const fetchHTML = async (url: string): Promise<string> => {
   } catch (error) {
     throw new Error(`Failed to fetch HTML: ${(error as Error).message}`);
   }
+};
+
+// add protocol if not present
+const normalizeUrl = (url: string): string => {
+  url = url.trim();
+
+  if (!/^[a-zA-Z]+:\/\//.exec(url)) {
+    return `https://${url}`;
+  }
+
+  return url;
 };
 
 export const fetchMultipleHTML = async (
