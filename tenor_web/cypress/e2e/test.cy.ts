f38d1b6c689089cdaac1801da6d7ffd5ae032fb9
Luis <lamadoalvarez@gmail.com>
2025-05-03 11:57:57 -0600

    First (dummy) test

--- tenor_web/cypress/e2e/test.cy.ts
+++ tenor_web/cypress/e2e/test.cy.ts
@@ -3,7 +3,14 @@ describe("hope", () => {
     cy.signIn("/");
   });
 
-  it("Testing", () => {
+  it("Create a project", () => {
+    // Dummy test example to verify the connection with auth and firestore
     cy.visit("/");
+    cy.get(".mr-10 > .justify-between > .flex").click();
+    cy.get('[placeholder="What is your project called..."]').type(
+      "Test project",
+    );
+    cy.get(".header > .flex").click();
+    cy.contains("Test project").should("be.visible");
   });
 });
