e641e9ea9977d30743157bf4318aea8a68052ced
Alonso Huerta <alonso12_284@outlook.com>
2025-05-04 23:42:34 -0600

    OHHOHOHOHOHO

--- tenor_web/cypress/e2e/tasks.cy.ts
+++ tenor_web/cypress/e2e/tasks.cy.ts
@@ -19,7 +19,7 @@ describe("Tasks", () => {
     cy.get('[data-cy="userStories"]').click();
   });
 
-  it("TC031: Create user story alert message", () => {
+  it("TC035: Create user story alert message", () => {
     cy.fixture("TestUserStory").then((data: TestUserStory) => {
       cy.get('[data-cy="primary-button"]').contains("+ New Story").click();
 
@@ -38,5 +38,33 @@ describe("Tasks", () => {
     });
 
     cy.get('[data-cy="primary-button"]').contains("+ Add task").click();
+    cy.get('[data-cy="primary-button"]').contains("Create Task").click();
+    cy.contains("Oops").should("be.visible");
+  });
+
+  it("TC036: Create task", () => {
+    cy.contains("US01").click();
+
+    cy.get('[data-cy="primary-button"]').contains("+ Add task").click();
+
+    cy.fixture("TestTask").then((data: TestTask) => {
+      cy.get('[data-cy="popup"]').within(() => {
+        cy.get('[placeholder="Enter task name..."]').type(data.name);
+        cy.get('[placeholder="Task description"]').type(data.description);
+
+        cy.get('[data-cy="primary-button"]').contains("Create Task").click();
+      });
+    });
+  });
+
+  it("TC038: Open tasks", () => {
+    cy.contains("US01").click();
+
+    cy.fixture("TestTask").then((data: TestTask) => {
+      cy.get('[data-cy="popup"]').within(() => {
+        cy.contains("TS01").click();
+        cy.contains(data.description).should("be.visible");
+      });
+    });
   });
 });
