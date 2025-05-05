68ebd5349a2bc2c84d367ab629b4f9d9a704ae9a
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-05-04 22:48:13 -0600

    fix bug

--- tenor_web/cypress/e2e/requirement-create.cy.ts
+++ tenor_web/cypress/e2e/requirement-create.cy.ts
@@ -1,6 +1,7 @@
 describe("Requirement Creation", () => {
   // Create a project to view it
   before(() => {
+    cy.signIn("/");
     cy.createEmptyProject();
   });
 
@@ -9,7 +10,7 @@ describe("Requirement Creation", () => {
   });
 
   it("TC010: Requirements pop up", () => {
-    cy.contains("Test project").click();
+    cy.contains("Test Project").click();
     cy.contains("Requirements").click();
     cy.get(".gap-1 > .bg-app-primary").click();
     cy.get('[data-cy="requirement-popup-footer"]').should("be.visible");
