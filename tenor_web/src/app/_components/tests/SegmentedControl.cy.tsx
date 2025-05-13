b1e223b302e247557a2a1925c4c9456e400ada34
luis r <luisresendez_@hotmail.com>
2025-05-13 14:35:01 -0600

    segmented control tests

---
+++ tenor_web/src/app/_components/tests/SegmentedControl.cy.tsx
@@ -0,0 +1,28 @@
+import { SegmentedControl } from "../SegmentedControl";
+import { mount } from "cypress/react";
+
+describe("SegmentedControl", () => {
+  it("renders", () => {
+    mount(
+      <SegmentedControl
+        options={["Option 1", "Option 2", "Option 3"]}
+        selectedOption={"Option 1"}
+        onChange={() => {}}
+      />,
+    );
+    cy.get("button").should("have.length", 3);
+  });
+
+  it("changes selected option on click", () => {
+    const onChange = cy.stub();
+    mount(
+      <SegmentedControl
+        options={["Option 1", "Option 2", "Option 3"]}
+        selectedOption={"Option 1"}
+        onChange={onChange}
+      />,
+    );
+    cy.get("button").contains("Option 2").click();
+    cy.wrap(onChange).should("have.been.calledWith", "Option 2");
+  });
+});
