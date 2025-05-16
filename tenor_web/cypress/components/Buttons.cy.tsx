a19203fb9dba4ca44aeac438c6477c4c5e34404d
Alonso Huerta <alonso12_284@outlook.com>
2025-05-16 13:35:53 -0600

    test click

--- tenor_web/cypress/components/Buttons.cy.tsx
+++ tenor_web/cypress/components/Buttons.cy.tsx
@@ -4,19 +4,25 @@ import SecondaryButton from "~/app/_components/buttons/SecondaryButton";
 
 describe("Buttons", () => {
   it("Test Primary Button", () => {
+    const onChange = cy.stub();
     mount(
-      <PrimaryButton onClick={() => {}} className="w-40">
+      <PrimaryButton onClick={onChange} className="w-40">
         Primary Button
       </PrimaryButton>,
     );
     cy.get("button").should("have.text", "Primary Button");
+    cy.get("button").click();
+    cy.wrap(onChange).should("have.been.called");
   });
   it("Test Secondary Button", () => {
+    const onChange = cy.stub();
     mount(
-      <SecondaryButton onClick={() => {}} className="w-40">
+      <SecondaryButton onClick={onChange} className="w-40">
         Secondary Button
       </SecondaryButton>,
     );
     cy.get("button").should("have.text", "Secondary Button");
+    cy.get("button").click();
+    cy.wrap(onChange).should("have.been.called");
   });
 });
