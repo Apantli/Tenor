84a34d97fb9e5bd3c9bcec76000d901aab434eec
NicoM3Nd0Z4 <a00835649@tec.mx>
2025-05-18 23:38:42 -0600

    Created the info overview of the project

--- tenor_web/src/app/(logged)/project/[projectId]/page.tsx
+++ tenor_web/src/app/(logged)/project/[projectId]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import ProjectInfo from "~/app/_components/sections/ProjectInfo";
 
 export default function ProjectOverview() {
 
@@ -8,9 +9,9 @@ export default function ProjectOverview() {
   const projectId = params.projectId as string;
 
   return (
-    <div className="">
-      <div className="flex w-2/4 flex-col gap-5 border-2 border-[#BECAD4] rounded-lg p-5">
-        
+    <div className="flex h-full w-full flex-row items-start">
+      <div className="flex w-2/4 flex-col gap-5 p-5">
+        <ProjectInfo projectId={projectId} />
       </div>
       <div className="flex w-2/4 flex-col gap-5 border-2 border-[#BECAD4] rounded-lg p-5">
         
---
+++ tenor_web/src/app/_components/sections/ProjectInfo.tsx
@@ -0,0 +1,72 @@
+import { api } from "~/trpc/react";
+import { useState } from "react";
+import LoadingSpinner from "../LoadingSpinner";
+import TertiaryButton from "../buttons/TertiaryButton";
+import Markdown from "react-markdown";
+
+
+export default function ProjectInfo({projectId}: {projectId: string}) {
+
+  const { data: project, isLoading } = api.projects.getGeneralConfig.useQuery({projectId});
+  const [showDescription, setShowDescription] = useState(false);
+  
+  const projectTitle = project?.name ?? "Project Name";
+  const projectDescription = project?.description ?? "Project Description";
+
+  // Define the length of the preview text
+  const previewLength = 255;
+
+  // Check if the description is longer than the preview length
+  // If it is, truncate it and add "..." at the end
+  const shouldTruncate = projectDescription.length > previewLength;
+  const previewText = shouldTruncate
+    ? projectDescription.slice(0, previewLength) + "..."
+    : projectDescription;
+
+  if (isLoading) {
+    return (
+      <div className="flex w-2/4 flex-col gap-5 border-2 border-[#BECAD4] rounded-lg p-5">
+        <LoadingSpinner />
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <div className="flex flex-row items-start gap-3 justify-start w-full">
+        <div className="h-[80px] w-[80px] min-w-[80px] items-center justify-center overflow-hidden rounded-md border-2 bg-white">
+          <img
+          src={
+            project?.logo
+              ? project.logo.startsWith("/")
+                ? project.logo
+                : `/api/image_proxy/?url=${encodeURIComponent(project.logo)}`
+              : ""
+          }
+          alt="Project Logo"
+          className="h-full w-full rounded-md object-contain p-[4px]"
+          />
+        </div>
+        <div>
+          <h1 className="text-2xl font-semibold">{projectTitle}</h1>
+        </div>
+      </div>
+      <div className="text-m text-gray-500">
+        <div className="flex flex-col gap-2 w-full">
+          <Markdown>
+            {showDescription ?? !shouldTruncate ? projectDescription : previewText}
+          </Markdown>
+        </div>
+        <div className="w-full flex justify-end">
+          {shouldTruncate && (
+            <TertiaryButton
+              onClick={() => setShowDescription(!showDescription)}
+            >
+              {showDescription ? "Read less" : "Read more"}
+            </TertiaryButton>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
