1fb275ad40b222c2520b1eb09031769bf6af324e
Luis <lamadoalvarez@gmail.com>
2025-05-19 13:47:22 -0600

    PR feedback comment

--- tenor_web/src/app/_hooks/usePersistentState.ts
+++ tenor_web/src/app/_hooks/usePersistentState.ts
@@ -3,6 +3,7 @@ import { useState } from "react";
 function usePersistentState<T>(initialValue: T, key: string) {
   const fullKey = `persistent_value:${key}`;
 
+  // Get the value from localStorage if it exists, otherwise use the initialValue
   const [value, setValue] = useState(
     typeof localStorage !== "undefined" && localStorage.getItem(fullKey)
       ? ((JSON.parse(localStorage.getItem(fullKey) ?? "") as T) ?? initialValue)
