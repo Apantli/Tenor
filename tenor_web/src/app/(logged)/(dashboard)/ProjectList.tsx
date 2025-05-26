a45f171b19cbea7fe8cc767136886e2bc766088b
Sergio Gonzalez Barraza <114702763+SergioGzzBrz@users.noreply.github.com>
2025-05-26 12:21:47 -0600

    Fix main project dashboard

---
+++ tenor_web/src/app/(logged)/(dashboard)/ProjectList.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { api } from "~/trpc/react";
+import { useRouter } from "next/navigation";
+import { useState, type ChangeEventHandler } from "react";
+import SearchBar from "~/app/_components/SearchBar";
+import LoadingSpinner from "~/app/_components/LoadingSpinner";
+import PrimaryButton from "~/app/_components/buttons/PrimaryButton";
+
+export default function ProjectList() {
+  const { data: projects, isLoading } = api.projects.listProjects.useQuery();
+  const [searchValue, setSearchValue] = useState("");
+  const router = useRouter();
+
+  const handleUpdateSearch: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchValue(e.target.value);
+  };
+  const handleOpenProject = (projectId: string) => {
+    router.push(`/project/${projectId}`);
+  };
+  const handleCreateProject = async () => {
+    router.push("/create-project");
+  };
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center">
+        <LoadingSpinner color="primary" />
+      </div>
+    );
+  }
+
+  const filteredProjects = projects?.filter((project) => {
+    return project.name.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
+  return (
+    <div className="mr-10">
+      <div className="flex h-full w-full justify-between gap-x-3 border-b-2 pb-3">
+        <SearchBar
+          searchValue={searchValue}
+          handleUpdateSearch={handleUpdateSearch}
+          placeholder="Find a project..."
+        />
+        <PrimaryButton
+          onClick={handleCreateProject}
+          data-cy="new-project-button"
+        >
+          {" "}
+          + New project{" "}
+        </PrimaryButton>
+      </div>
+      <ul
+        className="h-[calc(100vh-250px)] w-full overflow-hidden overflow-y-auto"
+        data-cy="project-list"
+      >
+        {filteredProjects && filteredProjects?.length > 0 ? (
+          filteredProjects?.map((project) => (
+            <li
+              className="flex flex-row justify-start border-b-2 py-[16px] pr-8 hover:cursor-pointer"
+              key={project.id}
+              onClick={() => handleOpenProject(project.id)}
+            >
+              <div className="h-[80px] w-[80px] min-w-[80px] items-center justify-center overflow-hidden rounded-md border-2 bg-white">
+                <img
+                  className="h-full w-full rounded-md object-contain p-[4px]"
+                  src={
+                    project.logo.startsWith("/")
+                      ? project.logo
+                      : `/api/image_proxy/?url=${encodeURIComponent(
+                          project.logo,
+                        )}`
+                  }
+                  alt={project.name}
+                />
+              </div>
+              <div className="flex flex-1 flex-col justify-start overflow-hidden pl-4 pr-4">
+                <h3 className="my-[7px] max-w-[500px] truncate text-lg font-semibold">
+                  {project.name}
+                </h3>
+                <p className="line-clamp-2 text-base">{project.description}</p>
+              </div>
+            </li>
+          ))
+        ) : (
+          <li className="flex h-full justify-start border-b-2">
+            <div className="py-[20px]">
+              <p className="text-gray-500">No projects found.</p>
+
+              {(projects?.length ?? 0) > 0 ? (
+                <p className="text-sm text-gray-500">
+                  Try changing the search query.
+                </p>
+              ) : (
+                <p className="text-sm text-gray-500">
+                  Try creating a project or ask a project owner to add you to a
+                  project.{" "}
+                </p>
+              )}
+            </div>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+}
