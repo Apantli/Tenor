d642f12b5c958d53e4afc8f9026cb91ce5ca1211
Alonso Huerta <alonso12_284@outlook.com>
2025-06-02 15:38:01 -0600

    dependency

--- tenor_web/src/app/(logged)/(dashboard)/page.tsx
+++ tenor_web/src/app/(logged)/(dashboard)/page.tsx
@@ -3,7 +3,6 @@
 import { ProjectStatusDashboard } from "~/app/(logged)/(dashboard)/ProjectStatusDashboard";
 import ProjectList from "./ProjectList";
 import ActivityProjectOverview from "~/app/_components/ActivityProjectOverview";
-import { api } from "~/trpc/react";
 import { useState } from "react";
 
 export default function ProjectPage() {
