aa68b18733e5646db06da020013eccc6d21893e4
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-06-01 18:14:06 -0600

    scroll if needed

--- tenor_web/src/app/(logged)/project/[projectId]/performance/page.tsx
+++ tenor_web/src/app/(logged)/project/[projectId]/performance/page.tsx
@@ -20,7 +20,7 @@ export default function ProjectPerformance() {
   const [selectedMember, setSelectedMember] = useState<UserCol | null>(null);
 
   return (
-    <div className="m-6 flex-1 p-4">
+    <div className="m-6 flex-1 overflow-y-auto p-4">
       <div className="flex h-full w-full flex-col gap-4 overflow-y-auto p-4 lg:flex-row lg:gap-16 xl:overflow-hidden">
         <div className="flex w-full flex-col items-baseline gap-3 pb-4 lg:w-[55%] lg:min-w-0 lg:flex-shrink">
           <div className="flex w-full flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
