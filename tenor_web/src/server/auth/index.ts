5c4641e3a49b9266d287863d9dd5034970f36431
Luis <lamadoalvarez@gmail.com>
2025-06-08 12:59:59 -0600

    Check user exists as well

--- tenor_web/src/server/auth/index.ts
+++ tenor_web/src/server/auth/index.ts
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import { firebaseAdmin } from "~/lib/db/firebaseAdmin";
 import { cache } from "react";
+import { getGlobalUserRef } from "../api/shortcuts/users";
 
 export async function uncachedAuth() {
   const cookieStore = await cookies();
@@ -13,6 +14,16 @@ export async function uncachedAuth() {
   try {
     const decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
     const user = await firebaseAdmin.auth().getUser(decodedToken.uid);
+
+    // Check user exists in database
+    const userDoc = await getGlobalUserRef(
+      firebaseAdmin.firestore(),
+      user.uid,
+    ).get();
+    if (!userDoc.exists) {
+      return null;
+    }
+
     return user;
   } catch (error) {
     if (typeof error === "object" && error != null && "code" in error) {
